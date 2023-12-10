cebfa97af000a8cb0796f15318354731472162b1
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-12-10 17:13:43 -0500

    Implements logical services: deseo, bad-words

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Deseo } from 'src/app/models/deseo';
+import { BadWordsService } from 'src/app/services/bad-words.service';
+import { DeseosService } from 'src/app/services/deseos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent {
+  deseo_in: string = '';
 
+  constructor(
+    public badwordsService: BadWordsService,
+    public desiresService: DeseosService,
+    private router: Router,
+  ) { }
+  ngOnInit(): void {
+  }
+  registrarDeseo() {
+    if (this.deseo_in.length > 0) {
+      let deseo = new Deseo(this.deseo_in)
+      this.desiresService.postDesire(deseo).subscribe((res) => {
+        if (res == true) {
+          this.deseo_in = '';
+          alert('¡Deseo registrado!');
+          this.router.navigate(['/clou-deseos']);
+        }else {
+          alert('Ingrese un deseo sin malas Palabras >:/');
+        }
+      })
+    } else {
+      alert('Debes ingresar tu deseo antes de poder enviarlo')
+    }
+  }
 }
